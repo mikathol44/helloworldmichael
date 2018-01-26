896081efa2e3b41033a38c6213750f10b98ffaf8
mikathol44 <35847972+mikathol44@users.noreply.github.com>
2018-01-26 16:58:08 -0600

    Add files via upload

--- helloName.c
+++ helloName.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-	printf("Robert Kirkpatrick "); //change 
+	printf("Robert Kirkpatrick and Michael Kathol "); //change by Michael Kathol
 	
 	return 0;
 }
